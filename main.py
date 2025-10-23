ef595a33b5b9d4e7fba5394258901beb6d357a1a
Davydov Daniil <davydovdaniil501@gmail.com>
2025-10-23 20:45:52 +0300

    Add both models training (WIP)

--- main.py
+++ main.py
@@ -14,39 +14,61 @@ def parse_args():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='cmd')
 
-    default_raw_data_path = Path('toxic_clf/models/code-review-dataset-full.xlsx')
-    default_prepared_data_path = Path('toxic_clf/models/prepared-code-review-dataset-full')
+    default_raw_train_data_path = Path('toxic_clf/models/code-review-dataset-full.xlsx')
+    default_raw_test_data_path = Path('toxic_clf/models/test.xlsx')
+    default_prepared_train_data_path = Path('toxic_clf/models/prepared-train-code-review-dataset-full')
+    default_prepared_test_data_path = Path('toxic_clf/models/prepared-test-code-review-dataset-full')
     prepare_data_parser = subparsers.add_parser('prepare-data')
     prepare_data_parser.set_defaults(func=prepare_data)
     prepare_data_parser.add_argument(
-        'input',
+        'input_train',
         nargs='?',
         help='Path to load raw dataset',
         type=Path,
-		default=default_raw_data_path,
+		default=default_raw_train_data_path,
     )
     prepare_data_parser.add_argument(
-        '-o',
-        '--output',
+        'input_test',
+        nargs='?',
+        help='Path to load raw dataset',
+        type=Path,
+		default=default_raw_test_data_path,
+    )
+    prepare_data_parser.add_argument(
+        '--output_train',
         nargs='?',
         help='Path to save prepared dataset to',
         type=Path,
-        default=default_prepared_data_path,
+        default=default_prepared_train_data_path,
+    )
+    prepare_data_parser.add_argument(
+        '--output_test',
+        nargs='?',
+        help='Path to save prepared dataset to',
+        type=Path,
+        default=default_prepared_test_data_path,
     )
 
     predict_parser = subparsers.add_parser('classify')
     predict_parser.set_defaults(func=classify)
     predict_parser.add_argument(
-        '-d',
-        '--dataset',
+        '--train_dataset',
+        nargs='?',
         help='Path to prepared dataset',
         type=Path,
-        default=default_prepared_data_path,
+        default=default_prepared_train_data_path,
+    )
+    predict_parser.add_argument(
+        '--test_dataset',
+        nargs='?',
+        help='Path to prepared dataset',
+        type=Path,
+        default=default_prepared_test_data_path,
     )
     predict_parser.add_argument(
         '-m',
         '--model',
-        choices=['classic_ml', 'microsoft/codebert-base'],
+        choices=['classic_ml', 'roberta'],
         default='classic_ml',
     )
 
@@ -54,13 +76,16 @@ def parse_args():
 
 
 def prepare_data(args):
-    dataset = prepare(args.input)
-    save_dataset(dataset, args.output)
+    train_dataset = prepare(args.input_train)
+    test_dataset = prepare(args.input_test)
+    save_dataset(train_dataset, args.output_train)
+    save_dataset(test_dataset, args.output_test)
 
 
 def classify(args):
-    dataset = load_dataset(args.dataset)
-    classifier(dataset, args.model)
+    train_dataset = load_dataset(args.train_dataset)
+    test_dataset = load_dataset(args.test_dataset)
+    classifier(train_dataset, test_dataset, args.model)
 
 
 if __name__ == '__main__':
--- toxic_clf/models.py
+++ toxic_clf/models.py
@@ -1,20 +1,105 @@
-from statistics import mean, stdev
-
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
-from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_recall_fscore_support
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+def classic_ml(train_dataset, test_dataset):
+    train_df = train_dataset.to_pandas()
+    test_df = test_dataset.to_pandas()
+
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_features=19000)
+    X_train = vectorizer.fit_transform(train_df["message_clean"])
+    X_test = vectorizer.transform(test_df["message_clean"])
+    y_train = train_df["is_toxic"]
+    y_test = test_df["is_toxic"]
+
+    clf = LogisticRegression(max_iter=2000, class_weight="balanced")
+    clf.fit(X_train, y_train)
+
+    kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+    cv_scores = cross_val_score(clf, X_train, y_train, cv=kf, scoring='f1_weighted', n_jobs=-1)
+
+    print(f"10-fold cross-validation F1-scores: {cv_scores}")
+    print(f"Mean CV F1-score: {np.mean(cv_scores)}")
+
+    y_pred = clf.predict(X_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Non-toxic", "Toxic"])
+    disp.plot(cmap=plt.cm.Blues)
+    plt.title("Confusion Matrix")
+
+    plt.tight_layout()
+    plt.savefig("toxic_clf/models/confusion_matrix.png")
+    plt.close()
+
+    return
+
+def roberta(train_dataset, test_dataset):
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+
+    def tokenize_function(examples):
+        result = tokenizer(
+            examples["message_clean"],
+            padding="max_length",
+            truncation=True,
+            max_length=64 # с этим можно поиграться
+        )
+        result["labels"] = examples["is_toxic"]
+        return result
+
+    tokenized_train = train_dataset.map(tokenize_function, batched=True)
+    tokenized_test = test_dataset.map(tokenize_function, batched=True)
+
+    model = AutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=2)
+
+    training_args = TrainingArguments(
+        output_dir="./roberta_toxicity",
+        num_train_epochs=2,
+        per_device_train_batch_size=128,
+        per_device_eval_batch_size=128,
+        learning_rate=5e-5,
+        metric_for_best_model="f1",
+        save_total_limit=2
+    )
+
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average="weighted")
+        acc = accuracy_score(labels, preds)
+        return {
+            'accuracy': acc,
+            'precision': precision,
+            'recall': recall,
+            'f1': f1
+        }
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+
+    trainer.train()
+
+    model.save_pretrained("toxic_clf/models/roberta-pretrainded")
+    tokenizer.save_pretrained("toxic_clf/models/roberta-pretrainded")
+
+    return
 
 
-def classifier(dataset, model):
-    # Use real X and y from dataset
-    X = np.random.rand(100, 3)
-    y = np.random.randint(2, size=100)
-
-    skf = StratifiedKFold(n_splits=10, random_state=42, shuffle=True)
-    scores = []
-    for train_index, test_index in tqdm(skf.split(X, y)):
-        # Fit and evaluate model on each fold
-        pass
-
-    print('Some pretty description:')
-    print(mean(scores), stdev(scores))
+def classifier(train_dataset, test_dataset, model):
+    if (model == "classic_ml"):
+        classic_ml(train_dataset, test_dataset)
+    elif (model == "roberta"):
+        roberta(train_dataset, test_dataset)
+    else:
+        raise ValueError(f"Invalid model parameter: {model}")
